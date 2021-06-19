af48abad7a947b9d326b6589068a99edf4f18a43
LucasSurPhP <75563988+LucasSurPhP@users.noreply.github.com>
2021-06-19 16:38:44 +0200

    3 commit

--- src/Minage/Main.php
+++ src/Minage/Main.php
@@ -2,4 +2,32 @@
 
 namespace Minage;
 
-use pocketmine\Command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\{Server, Player};
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\math\Vector3;
+use pocketmine\level\Position;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable() {
+        $this->getServer()->getLogger()->info("§aPlugin Minage on");
+            $this->getserver()->getPluginManage()->registerEvents($this, this);
+
+      Server::getInstance()->loadLevel("Minage");
+
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    {
+        if($command->getName() === "minage") {
+            if($sender instanceof Player) {
+
+                $level = Server::getInstance()->getLevelByName("Minage");
+            }
+        }
+    }
+
+
+}
